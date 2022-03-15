a64b6d6ec9f086600d36e1c565ecbbf0b9ad5b3d
Trezedo <1962234583@qq.com>
2022-03-15 17:20:00 +0800

    deploy.md

--- deploy.cmd
+++ deploy.cmd
@@ -2,9 +2,11 @@
 :: ch+cp 65001
 echo 进入 /dist 部署
 
+@REM git push gitee master:theme-hope
+@REM git branch master
+
 git checkout pages
-@REM cd dist
+@REM git rm -r --cached * @REM 删除已经提交的文件
 git add dist -f
 git commit -m "build"
 git push gitee pages:pages
-git checkout master
