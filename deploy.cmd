5285a3b2c5ed609224b1aa498d0948bc2d355648
Trezedo <1962234583@qq.com>
2022-03-15 21:50:22 +0800

    add 'and' control for deploy.cmd

--- deploy.cmd
+++ deploy.cmd
@@ -6,12 +6,13 @@ echo 进入 /dist 部署
 @REM 逻辑：添加文件并推送，然后取消所有提交的文件并无视（为了下次打包过来直接推送，因为当前分支不能 build）
 
 @REM yarn docs:build
-git checkout pages
+git checkout pages && deploy.cmd
+
 @REM git rm -r --cached * @REM 删除已经提交的文件追踪，并不会物理删除
 @REM git rm -r dist/assets/js
-git add dist
-git commit -m "build and deploy"
-git push gitee pages:pages
-git rm -r dist
-git commit -m "remove build after 'push'"
-git checkout master
+@REM git add dist
+@REM git commit -m "build and deploy"
+@REM git push gitee pages:pages
+@REM git rm -r dist
+@REM git commit -m "remove build after 'push'"
+@REM git checkout master
