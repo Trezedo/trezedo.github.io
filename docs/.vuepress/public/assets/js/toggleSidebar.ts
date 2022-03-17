34231d7958de51132646e3080c56dffd51c69cb3
Trezedo <1962234583@qq.com>
2022-03-17 19:31:59 +0800

    add a toggle sidebar button, with icon, and transitions

--- docs/.vuepress/public/assets/js/toggleSidebar.ts
+++ docs/.vuepress/public/assets/js/toggleSidebar.ts
@@ -4,19 +4,25 @@
     if (sidebar) {
         const tButton = document.createElement("button");
         tButton.className = "toggle-sidebar";
+        tButton.innerHTML = `<i class="icon iconfont icon-sidebar" style="font-size: 28px"></i>`
+        tButton.setAttribute("aria-label", "隐藏/显示侧边栏");
+        tButton.setAttribute("data-balloon-pos", "left");
+
         let isShow = true;
         setTimeout(() => {
             document.body.appendChild(tButton)
         }, 200)
         tButton.addEventListener('click', () => {
             if (isShow) {
-                sidebar.style.visibility = 'hidden'
+                sidebar.classList.add('hide')
                 // var(--sidebar-width)
                 mainPage.style.paddingLeft = '0';
             } else {
-                sidebar.style.visibility = 'visible'
+                // sidebar.style.left = 'var(--sidebar-width)'
+                sidebar.classList.remove('hide')
                 // var(--sidebar-width)
-                mainPage.style.paddingLeft = 'var(--sidebar-width)';
+                mainPage.removeAttribute('style')
+                // mainPage.style.paddingLeft = ''; // var(--sidebar-width)
             }
             isShow = !isShow
         })
@@ -25,5 +31,5 @@
 
 /**
  tsc docs/.vuepress/public/assets/js/toggleSidebar.ts
-
+ uglifyjs docs/.vuepress/public/assets/js/toggleSidebar.js -o docs/.vuepress/public/assets/js/toggleSidebar.js -m
  */
