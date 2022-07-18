6c1f6d9789fd7b3dc733fc3076664600eb90014e
zedo <1962234583@qq.com>
2022-07-18 22:12:42 +0800

    style💄: footer with blur

--- custom/hooks/useBackground.ts
+++ custom/hooks/useBackground.ts
@@ -15,7 +15,7 @@ export function useBackground(test?: (path: string) => boolean) {
     const install = () => {
         bg.style.visibility = "visible";
         css.innerHTML = `
-        .blog-hero.fullscreen .mask{background-attachment:fixed !important}#bg{position:fixed;top:0;left:0;width:100%;height:100%;z-index:-1;background:url(https://w.wallhaven.cc/full/72/wallhaven-72rd8e.jpg) center center/cover no-repeat;pointer-events:none}body,.theme-container .page.blog:not(p){background:transparent}.pagination-wrapper:not(p){margin-bottom:1.25rem;border-radius:0.4rem;background:var(--bg-color-blur)}.article-type-wrapper:not(p){padding:1rem;border-radius:0.4rem;background:var(--bg-color-blur)}.tag-list-wrapper .tag:not(p){box-shadow:none}
+        .blog-hero.fullscreen .mask{background-attachment:fixed !important}#bg{position:fixed;top:0;left:0;width:100%;height:100%;z-index:-1;background:url(https://w.wallhaven.cc/full/72/wallhaven-72rd8e.jpg) center center/cover no-repeat;pointer-events:none}body,.theme-container .page.blog:not(p){background:transparent}.footer-wrapper{background:var(--bg-color-blur)}.pagination-wrapper:not(p){margin-bottom:1.25rem;border-radius:0.4rem;background:var(--bg-color-blur)}.article-type-wrapper:not(p){padding:1rem;border-radius:0.4rem;background:var(--bg-color-blur)}.tag-list-wrapper .tag:not(p){box-shadow:none}
     `;
     };
 
