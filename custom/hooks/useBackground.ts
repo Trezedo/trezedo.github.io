18a21ce09f18e3a016fb2ef1fb4548be53665ccb
zedo <1962234583@qq.com>
2022-07-19 00:26:38 +0800

    perf👌: add transition

--- custom/hooks/useBackground.ts
+++ custom/hooks/useBackground.ts
@@ -19,8 +19,8 @@ export function useBackground(
         // bg.style.visibility = "visible"; 为了兼容，当图片不可用时显示 bgc
         bg.style.backgroundImage = `url(${img})`;
         css.innerHTML = `
-        .blog-hero.fullscreen .mask{background-attachment:fixed !important}#bg{position:fixed;top:0;left:0;width:100%;height:100%;z-index:-1;pointer-events:none;background:var(--bg-color-back) center center/cover no-repeat}body,.theme-container .page.blog:not(p){background:transparent}.footer-wrapper{background:var(--bg-color-blur)}.pagination-wrapper:not(p){margin-bottom:1.25rem;border-radius:0.4rem;background:var(--bg-color-blur)}.article-type-wrapper:not(p){padding:1rem;border-radius:0.4rem;background:var(--bg-color-blur)}.tag-list-wrapper .tag:not(p){box-shadow:none}
-    `.trim();
+        .blog-hero.fullscreen .mask{background-attachment:fixed !important}#bg{position:fixed;top:0;left:0;width:100%;height:100vh;z-index:-1;pointer-events:none;background:var(--bg-color-back) center center/cover no-repeat}#bg{height:100vh;transition:all 1s ease}body,.theme-container .page.blog:not(p){background:transparent}.footer-wrapper{background:var(--bg-color-blur)}.pagination-wrapper:not(p){margin-bottom:1.25rem;border-radius:0.4rem;background:var(--bg-color-blur)}.article-type-wrapper:not(p){padding:1rem;border-radius:0.4rem;background:var(--bg-color-blur)}.tag-list-wrapper .tag:not(p){box-shadow:none}
+        `.trim();
     };
 
     const uninstall = () => {
