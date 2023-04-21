db37becd6c747ed017fc0f6c8948211349dc69ce
zedo <1962234583@qq.com>
2023-04-21 22:59:52 +0800

    style💄: 大屏时取消字体放大为 18px

--- docs/.vuepress/configs/plugins.ts
+++ docs/.vuepress/configs/plugins.ts
@@ -4,6 +4,7 @@ import { registerComponents } from "./plugins/registerComponents";
 import search from "./plugins/search";
 import type { ComponentOptions } from "vuepress-plugin-components";
 import type { CommentOptions } from "vuepress-plugin-comment2";
+import { isDev } from "../config";
 
 export const pluginConfig: PluginConfig = [
     //
@@ -11,22 +12,34 @@ export const pluginConfig: PluginConfig = [
     search,
 ];
 
+function getDate() {
+    const today = new Date();
+    const year = today.getFullYear();
+    const month = today.getMonth() + 1;
+    const day = today.getDate();
+    return `${year}-${month}-${day}`;
+}
+
 // 主题中不能单独使用
 export const components: ComponentOptions = {
     rootComponents: {
+        // https://plugin-components.vuejs.press/zh/guide/notice.html
         notice: [
             {
                 path: "/",
-                title: "公告",
+                title: "最近更新",
                 content: [
                     // "新增公告组件，更新部分文章的笔误，本地降至 pnpm7 以保证和 netlify 的版本一致",
                     // "修改 springboot 笔记，尝试通过 packageManager 使用 pnpm8",
                     // "新增 netlify 部署笔记，更新收集的在线工具",
-                    "启用 Giscus 评论区，新增 minio 笔记",
+                    // "启用 Giscus 评论区，新增 minio 笔记",
+                    `1.更正 MySQL 文章中的勘误，补充 MinIO。
+                    <br>
+                    2.使用 shields.io 展示库/包的版本号。`,
                 ][0],
                 // confirm: true,
                 // actions: [],
-                // showOnce: false,
+                showOnce: isDev() ? false : true,
                 // fullscreen: true,
             },
         ],
